d9e053efb5ef0f11e432c5305a56eeed23bdb14d
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2022-12-16 01:51:23 +0100

    please work

--- memory_mangament.c
+++ memory_mangament.c
@@ -1,2 +1,29 @@
+#include "monty.h"
+/**
+ * free_info - free data
+ *
+ *
+ */
+void free_info(void)
+{
+	free(info.cmd);
+	info.cmd = NULL;
+	free(info.arg);
+	info.arg = NULL;
+	fclose(info.fp);
+}
+/**
+ * free_list - free list
+ * @stack: stack or queue
+ */
+void free_list(stack_t *stack)
+{
+	stack_t *node;
 
-idfjdn
+	while (stack)
+	{
+		node = stack;
+		stack = stack->next;
+		free(node);
+	}
+}
